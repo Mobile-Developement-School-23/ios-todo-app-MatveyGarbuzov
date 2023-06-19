ea64d3ab34107c34ff0af6516484906e3334675a
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-06-19 12:35:35 +0300

    Add custom labels

---
+++ ios-todo-app-MatveyGarbuzov/Extensions/CustomLabels.swift
@@ -0,0 +1,88 @@
+//
+//  CustomLabels.swift
+//  ios-todo-app-MatveyGarbuzov
+//
+//  Created by Matvey Garbuzov on 19.06.2023.
+//
+
+import UIKit
+
+extension UILabel {
+  static func largeTitle(with text: String) -> UILabel {
+    let label = UILabel()
+    label.frame = CGRect(x: 0, y: 0, width: 0, height: 46)
+    label.textColor = .aLabelPrimary
+    
+    label.font = .systemFont(ofSize: 38, weight: .init(rawValue: 700))
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1
+    label.attributedText = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.kern: 0.42, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    
+    return label
+  }
+  
+  static func title(with text: String) -> UILabel {
+    let label = UILabel()
+    label.frame = CGRect(x: 0, y: 0, width: 0, height: 24)
+    label.textColor = .aLabelPrimary
+    
+    label.font = .systemFont(ofSize: 20, weight: .init(rawValue: 600))
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1
+    label.attributedText = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.kern: 0.38, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    
+    return label
+  }
+  
+  static func headline(with text: String) -> UILabel {
+    let label = UILabel()
+    label.frame = CGRect(x: 0, y: 0, width: 0, height: 22)
+    label.textColor = .aLabelPrimary
+    
+    label.font = .systemFont(ofSize: 17, weight: .init(rawValue: 600))
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1
+    label.attributedText = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.kern: -0.41, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    
+    return label
+  }
+  
+  static func body(with text: String) -> UILabel {
+    let label = UILabel()
+    label.frame = CGRect(x: 0, y: 0, width: 0, height: 22)
+    label.textColor = .aLabelPrimary
+    
+    label.font = .systemFont(ofSize: 17, weight: .init(rawValue: 400))
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1
+    label.attributedText = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.kern: -0.41, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    
+    return label
+  }
+  
+  static func subhead(with text: String) -> UILabel {
+    let label = UILabel()
+    label.frame = CGRect(x: 0, y: 0, width: 0, height: 20)
+    label.textColor = .aLabelPrimary
+    
+    label.font = .systemFont(ofSize: 15, weight: .init(rawValue: 400))
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1
+    label.attributedText = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.kern: -0.24, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    
+    return label
+  }
+  
+  static func footnote(with text: String) -> UILabel {
+    let label = UILabel()
+    label.frame = CGRect(x: 0, y: 0, width: 0, height: 18)
+    label.textColor = .aLabelPrimary
+    
+    label.font = .systemFont(ofSize: 13, weight: .init(rawValue: 600))
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineHeightMultiple = 1
+    label.attributedText = NSMutableAttributedString(string: text, attributes: [NSAttributedString.Key.kern: -0.08, NSAttributedString.Key.paragraphStyle: paragraphStyle])
+    
+    return label
+  }
+}
