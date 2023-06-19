5205f2e56030e18d40dcf6b93d30566d4a9eed72
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-06-19 10:10:51 +0300

    Add custom colors

---
+++ ios-todo-app-MatveyGarbuzov/Extensions/CustomColors.swift
@@ -0,0 +1,36 @@
+//
+//  CustomColors.swift
+//  ios-todo-app-MatveyGarbuzov
+//
+//  Created by Matvey Garbuzov on 19.06.2023.
+//
+
+import UIKit
+
+// MARK: Custom colors
+extension UIColor {
+  // MARK: Support
+  static let aNavBarBlur = UIColor(named: "NavBarBlur")
+  static let aOverlay = UIColor(named: "Overlay")
+  static let aSeparator = UIColor(named: "Separator")
+  
+  // MARK: Label
+  static let aLabelDisable = UIColor(named: "LabelDisable")
+  static let aLabelPrimary = UIColor(named: "LabelPrimary")
+  static let aLabelSecondary = UIColor(named: "LabelSecondary")
+  static let aLabelTertiary = UIColor(named: "LabelTertiary")
+  
+  // MARK: Color
+  static let aBlue = UIColor(named: "Blue")
+  static let aGray = UIColor(named: "Gray")
+  static let aGrayLight = UIColor(named: "GrayLight")
+  static let aGreen = UIColor(named: "Green")
+  static let aRed = UIColor(named: "Red")
+  static let aWhite = UIColor(named: "White")
+  
+  // MARK: Back
+  static let aBackElevated = UIColor(named: "BackElevated")
+  static let aBackIOSPrimary = UIColor(named: "BackIOSPrimary")
+  static let aBackPrimary = UIColor(named: "BackPrimary")
+  static let aBackSecondary = UIColor(named: "BackSecondary")
+}
