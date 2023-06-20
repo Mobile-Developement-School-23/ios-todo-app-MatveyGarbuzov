5638b5a006ea0d875529cf7f7ae4b190994b3ab4
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-06-20 11:38:58 +0300

    Importance horizontal stack is done

--- ios-todo-app-MatveyGarbuzov/Custom Views/Detail Screen/ImportanceHorizontalStack.swift
+++ ios-todo-app-MatveyGarbuzov/Custom Views/Detail Screen/ImportanceHorizontalStack.swift
@@ -7,25 +7,43 @@
 
 import UIKit
 
-final class ImportanceHorizontalStack: UIStackView {
+final class ImportanceHorizontalStack: UIView {
   
-  lazy private var importanceLabel: UILabel = {
-    let label = UILabel.body(with: "Важность")
+  lazy private var hStack: UIStackView = {
+    let stack = UIStackView()
+    stack.axis = .horizontal
+    stack.distribution = .fill
     
-    return label
+    return stack
   }()
   
-//  lazy private var segmentedControl: UISegmentedControl = {
-//    let unimportantImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 11, height: 14))
-//    unimportantImage.image = UIImage(
-//    let normal
-//    let important
-//
-//    let segmentedControl = UISegmentedControl(items: <#T##[Any]?#>)
-//    segmentedControl.frame = CGRect(x: 0, y: 0, width: 150, height: 36)
-//
-//    return segmentedControl
-//  }()
+  lazy private var importanceLabel = UILabel.body(with: "Важность")
+  
+  lazy private var segmentedControl: UISegmentedControl = {
+    let segmentedControl = UISegmentedControl()
+    
+    let unimportantImage = UIImage(named: "Unimportant")?.resized(to: CGSize(width: 11, height: 14))
+    let unimportantImageView = UIImageView(image: unimportantImage)
+    unimportantImageView.contentMode = .scaleAspectFit
+    
+    let importantImage = UIImage(named: "Important")?.resized(to: CGSize(width: 10, height: 16))
+    
+    let importantImageView = UIImageView(image: importantImage)
+    importantImageView.contentMode = .scaleAspectFit
+    
+    segmentedControl.insertSegment(with: unimportantImageView.image, at: 0, animated: false)
+    segmentedControl.insertSegment(withTitle: "нет", at: 1, animated: false)
+    segmentedControl.insertSegment(with: importantImageView.image, at: 2, animated: false)
+       
+    segmentedControl.selectedSegmentIndex = 1
+    
+    // TODO: Исправить цвет на .aOverlay или оставить .black
+    // (дизайн в figma расходится с отображением на симуляторе)
+    segmentedControl.backgroundColor = .black//.aOverlay
+    segmentedControl.selectedSegmentTintColor = .aBackElevated
+    
+    return segmentedControl
+  }()
   
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -37,10 +55,40 @@ final class ImportanceHorizontalStack: UIStackView {
   }
   
   private func customInit() {
-    
+    setup()
+    setupConstraints()
   }
   
   private func setupConstraints() {
+    addSubview(hStack)
     
+    hStack.addArrangedSubview(importanceLabel)
+    hStack.addArrangedSubview(segmentedControl)
+    
+    hStack.snp.makeConstraints { make in
+      make.leading.trailing.equalToSuperview().inset(12)
+      make.top.bottom.equalToSuperview().inset(10)
+    }
+    
+    segmentedControl.snp.makeConstraints { make in
+      make.top.bottom.equalToSuperview()
+      make.trailing.equalToSuperview()
+      make.width.equalTo(150)
+    }
+  }
+  
+  private func setup() {
+    backgroundColor = .aBackSecondary
+    layer.cornerRadius = 15
+  }
+}
+
+extension UIImage {
+  func resized(to size: CGSize) -> UIImage? {
+    let rect = CGRect(origin: .zero, size: size)
+    UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+    self.draw(in: rect)
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    return newImage?.withRenderingMode(.alwaysOriginal)
   }
 }
