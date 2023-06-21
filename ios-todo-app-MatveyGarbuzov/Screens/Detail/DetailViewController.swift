dae04ea2dc18041b6a4adef582c483d4c4f9aed1
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-06-21 13:26:20 +0300

    Add stack and delete button in DetailVC

--- ios-todo-app-MatveyGarbuzov/Screens/Detail/DetailViewController.swift
+++ ios-todo-app-MatveyGarbuzov/Screens/Detail/DetailViewController.swift
@@ -16,6 +16,22 @@ final class DetailViewController: UIViewController {
   }()
 
   private let textView = CustomTextView()
+  private let vStack = DetailVerticalStack()
+  
+  private lazy var deleteButton: UIButton = {
+    var configuration = UIButton.Configuration.filled()
+    configuration.attributedTitle = AttributedString(
+      "Удалить", attributes: AttributeContainer([NSAttributedString.Key.font : UIFont.aBody])
+    )
+    configuration.baseForegroundColor = .aLabelTertiary
+    configuration.baseBackgroundColor = .aBackSecondary
+    configuration.background.cornerRadius = 16
+    configuration.cornerStyle = .fixed
+    
+    let deleteButton = UIButton(configuration: configuration)
+    
+    return deleteButton
+  }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -38,11 +54,24 @@ final class DetailViewController: UIViewController {
   
   private func setupConstraints() {
     view.addSubview(textView)
+    view.addSubview(vStack)
+    view.addSubview(deleteButton)
     
     textView.snp.makeConstraints { make in
-      make.top.equalTo(view.safeAreaLayoutGuide)
+      make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
       make.leading.trailing.equalToSuperview().inset(16)
-      make.height.greaterThanOrEqualTo(100)
+      make.height.greaterThanOrEqualTo(120)
+    }
+    
+    vStack.snp.makeConstraints { make in
+      make.top.equalTo(textView.snp.bottom).offset(16)
+      make.leading.trailing.equalToSuperview().inset(16)
+    }
+    
+    deleteButton.snp.makeConstraints { make in
+      make.top.equalTo(vStack.snp.bottom).offset(16)
+      make.leading.trailing.equalToSuperview().inset(16)
+      make.height.equalTo(56)
     }
   }
   
