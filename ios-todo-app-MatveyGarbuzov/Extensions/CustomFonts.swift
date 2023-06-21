de9c00349cd6e3f13949ba8a9cb139c720ed73df
MatveyGarbuzov <matveyvoronezh@mail.ru>
2023-06-21 13:26:36 +0300

    Add custom fonts

---
+++ ios-todo-app-MatveyGarbuzov/Extensions/CustomFonts.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFonts.swift
+//  ios-todo-app-MatveyGarbuzov
+//
+//  Created by Matvey Garbuzov on 21.06.2023.
+//
+
+import UIKit
+
+extension UIFont {
+  static let aLargeTitle = UIFont.boldSystemFont(ofSize: 38)
+  static let aTitle = UIFont.boldSystemFont(ofSize: 20)
+  static let aHeadline = UIFont.boldSystemFont(ofSize: 17)
+  static let aBody = UIFont.systemFont(ofSize: 17)
+  static let aSubhead = UIFont.systemFont(ofSize: 15)
+  static let aFootnote = UIFont.boldSystemFont(ofSize: 13)
+}
